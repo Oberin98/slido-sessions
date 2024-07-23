d45768382689cebeb482307d524e25ac92d80153
Artem Beliaev <artembeliev98@gmail.com>
2024-07-23 21:59:00 +0200

    Add comments to selectors

--- src/entities/session/model/store/selectors.ts
+++ src/entities/session/model/store/selectors.ts
@@ -1,11 +1,17 @@
 import { SessionsStore } from './store';
 
+/**
+ * Get sessions from the store
+ */
 export function getSessionsSelector() {
   return (store: SessionsStore) => {
     return store.sessions;
   };
 }
 
+/**
+ * Get specific seesion from the store by id
+ */
 export function getSessionByIdSelector(id: string | number | undefined) {
   return (store: SessionsStore) => {
     if (!id) {
@@ -20,18 +26,27 @@ export function getSessionByIdSelector(id: string | number | undefined) {
   };
 }
 
+/**
+ * Get create session callback from the store
+ */
 export function getCreateSessionSelector() {
   return (store: SessionsStore) => {
     return store.createSession;
   };
 }
 
+/**
+ * Get update session callback from the store
+ */
 export function getUpdateSessionSelector() {
   return (store: SessionsStore) => {
     return store.updateSession;
   };
 }
 
+/**
+ * Get delete session callback from the store
+ */
 export function getDeleteSessionSelector() {
   return (store: SessionsStore) => {
     return store.deleteSession;
